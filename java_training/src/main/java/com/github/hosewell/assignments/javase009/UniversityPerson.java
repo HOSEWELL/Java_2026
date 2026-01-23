d1dd602933ae2bad55992723a0ba54ea33073944
HOSEWELL <hosewellkaranja@gmail.com>
2026-01-23 13:09:35 +0300

    inheritance assessment

---
+++ java_training/src/main/java/com/github/hosewell/assignments/javase009/UniversityPerson.java
@@ -0,0 +1,19 @@
+package com.github.hosewell.assignments.javase009;
+
+public class UniversityPerson {
+    protected String id;
+    protected String name;
+    protected String email;
+
+    public UniversityPerson(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Email: " + email;
+    }
+}
