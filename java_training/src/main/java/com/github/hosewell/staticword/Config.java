6ff539fc67d1f28a872b5735abf544f6a76bfe50
HOSEWELL <hosewellkaranja@gmail.com>
2026-01-27 08:20:19 +0300

    static key word

---
+++ java_training/src/main/java/com/github/hosewell/staticword/Config.java
@@ -0,0 +1,12 @@
+package com.github.hosewell.staticword;
+
+
+public class Config {
+
+    public final static String WORKING_DIR = "/user/hosewell/projects";
+
+    public final static String DB_PASSWORD = "12345";
+    public final static String DB_USERNAME = "12345ddfghjk";
+    public final static String DB_URL = "12345URl";
+
+}
