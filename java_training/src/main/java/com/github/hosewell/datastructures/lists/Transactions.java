dc7350220d319013b3067dc3a4e62cf0eca69b09
HOSEWELL <hosewellkaranja@gmail.com>
2026-02-04 06:44:20 +0300

    lists

--- java_training/src/main/java/com/github/hosewell/datastructures/lists/Transactions.java
+++
@@ -1,7 +0,0 @@
-package com.github.hosewell.datastructures.lists;
-
-public class Transactions {
-        private String debitAccount;
-        private String creditAccount;
-        private double amount;
-}
