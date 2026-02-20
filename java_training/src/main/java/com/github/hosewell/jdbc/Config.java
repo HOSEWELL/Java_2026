54c78b89d515788ebbca9e63c6d1aa6bb6c3d238
HOSEWELL <hosewellkaranja@gmail.com>
2026-02-20 12:25:14 +0300

    jdbc operations with postgreSQL and mysql commented out

---
+++ java_training/src/main/java/com/github/hosewell/jdbc/Config.java
@@ -0,0 +1,16 @@
+package com.github.hosewell.jdbc;
+
+public class Config {
+
+    public static final String MYSQL_DB_PASSWORD ="generic";
+    public static final String MYSQL_DB_USER ="root";
+    public static final String MYSQL_DB_URL ="jdbc:mysql://localhost:3306/training";
+
+
+    public static final String POSTGRES_DB_PASSWORD ="generic";
+    public static final String POSTGRES_DB_USER ="postgres";
+    public static final String POSTGRES_DB_URL ="jdbc:postgresql://localhost:5432/generic";
+
+
+
+}
